e690f08edb00661495450f7291e179c362c588a5
Usarneme <jamestcoop@gmail.com>
2021-03-24 16:20:41 -0700

    add some examples of the conditionals if else if, etc
    Co-authored-by: Rami Ahmed <ramiahmedportland@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,48 @@
+// $(document).ready(function() {
+
+// });
+
+// const letter = "a"
+
+// if (letter === "a") {
+//   // .. do thing 1
+
+//   // does stuff
+
+//   // skip else if else if to the very end
+// } else if (condition2) {
+//   // do something else
+
+//   // condition2 is true, do that, but then skip all the way to the end
+// } else if (condition3) {
+//   // do a third thing
+
+// }
+// // the very end
+// // from here
+
+// // {
+// //   "person": true,
+// //   "american": false
+// // }
+
+
+// if ("person" === true) {
+//   // do 1
+// }
+
+// if ("american" === true) {
+//   // do 2
+// }
+
+// if (condition3) {
+//   //do 3
+// }
+
+
+// if ("age" >= 18) {
+//   // display you can drive
+// } else {
+//   // you do not get to drive
+// }
+
